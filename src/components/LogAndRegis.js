5f810d266bd846d4d16c030a05d0435e3ac1a7da
CrocoXXX <lawrencenoman@gmail.com>
2023-07-10 00:36:43 +0700

    style and validate login register

--- src/components/LogAndRegis.js
+++ src/components/LogAndRegis.js
@@ -4,11 +4,11 @@ import styles from '../styles/LoginAndRegis.module.css'
 export default function LogAndRegis({ children }) {
   return (
     <div className='flex h-screen bg-blue-400'>
-      <div className='m-auto bg-slate-50 rounded-md w-3/5 h-3/4 grid lg:grid-cols-2'>
+      <div className='m-auto bg-white rounded-md w-3/5 h-3/4 grid lg:grid-cols-2'>
         <div className={styles.imgStyle}>
           <div className={styles.cartoonImg}></div>
         </div>
-        <div className={styles.formStyle} >
+        <div className={styles.formStyle}>
           <div className='text-center py-10'>
             {children}
           </div>
