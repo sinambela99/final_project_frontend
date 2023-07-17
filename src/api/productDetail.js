c3a9dc430aa2df6fb65a60b5acb7ec2e6aeae7d0
Satrio Hutomo <strhtmo17@gmail.com>
2023-07-18 04:46:57 +0700

    fetching data product by id (done)

---
+++ src/api/productDetail.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export async function getProductDetail(id) {
+  try {
+    const { data } = await axios.get(`http://localhost:8081/api/product-detail/${id}`);
+    // console.log(data.data);
+    return data.data;
+  } catch (err) {
+    // console.log(err);
+    return [];
+  }
+}
