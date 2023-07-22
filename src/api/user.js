0a41c0ac0ce43b3ace62d46b66672b14cf13e62e
arifbp98 <arifbudianto62@yahoo.co.id>
2023-07-22 21:55:15 +0700

    add headers

--- src/api/user.js
+++ src/api/user.js
@@ -2,34 +2,47 @@ import axios from "axios";
 
 export async function getUser(id) {
   try {
-    const { data } = await axios.get(`http://localhost:8081/api/user/${id}`);
+    const { data } = await axios.get(`http://localhost:8081/api/user/${id}`, {
+      headers: {
+        access_token: localStorage.getItem("access_token"),
+      },
+    });
     return data.data;
   } catch (err) {
     console.log(err);
-    return [];
   }
 }
 
 export async function updateUser(id, name, address, email) {
   try {
-    const { data } = await axios.put(`http://localhost:8081/api/user/${id}`, {
-      name,
-      address,
-      email,
-    });
+    const { data } = await axios.put(
+      `http://localhost:8081/api/user/${id}`,
+      {
+        name,
+        address,
+        email,
+      },
+      {
+        headers: {
+          access_token: localStorage.getItem("access_token"),
+        },
+      }
+    );
     return data.data;
   } catch (err) {
     console.log(err);
-    return [];
   }
 }
 
 export async function deleteUser(id) {
   try {
-    await axios.delete(`http://localhost:8081/api/user/${id}`);
+    await axios.delete(`http://localhost:8081/api/user/${id}`, {
+      headers: {
+        access_token: localStorage.getItem("access_token"),
+      },
+    });
     return;
   } catch (err) {
     console.log(err);
-    return [];
   }
 }
